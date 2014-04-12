1f6bdcdf393535e35d2ccd7801dbf2e8c95726ef
juliet <juliet.zhu.work@gmail.com>
2014-04-12 23:39:53 +0800

    fix error_tip.

--- dev/js/components/tools.js
+++ dev/js/components/tools.js
@@ -1,17 +1,17 @@
 window.tools={
   getTips:function(selector){
-            var lis = $(selector).find('li');
-            if(lis.length){
-              $('.note').hide();
-              $('.error').hide();
-              lis.each(function(index){
-                var field = $(this).attr('field');
-                var value = $(this).html();
-                var $append_selector = $('[name='+field+']').parent();
-                $('<label/>').attr('class','error').attr('for',field).html(value).insertAfter($append_selector);   
-              }); 
-            }else{
-              return;
-            }
-      }
+    var lis = $(selector).find('li');
+    if(lis.length){
+      $('.note').hide();
+      $('.error').hide();
+      lis.each(function(index){
+        var field = $(this).attr('field');
+        var value = $(this).html();
+        var $append_selector = $('[name='+field+']').parent();
+        $('<label/>').attr('class','error').attr('for',field).html(value).insertAfter($append_selector);   
+      }); 
+    }else{
+      return;
+    }
+  }
 }
